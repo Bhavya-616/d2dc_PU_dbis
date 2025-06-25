0593720e9b78d1a505978633b5bf31997da512d9
Bhavya-616 <152218611+Bhavya-616@users.noreply.github.com>
2025-06-25 15:57:37 +0530

    Add files via upload

---
+++ crud2.sql
@@ -0,0 +1,20 @@
+-- create database d2dc;
+use d2dc;
+-- create table Student(id int,st_name text,age int);
+-- insert into Student(id,st_name,age) values (1,'sumit',23);
+-- select * from Student;
+-- insert into Student(id,st_name,age) values
+-- (2,'amit',35),
+-- (3,'pradip',55),
+-- (4,'mark',34);
+
+-- insert into Student(id,st_name) values
+-- (11,'sumit'),
+-- (17,'jack');
+
+-- update Student set st_name='bruce' where id = 1;
+-- update Student set age = 50 where st_name ='bruce';
+
+-- delete from Student where id = 2;
+-- delete from Student where st_name ='pradip';
+
